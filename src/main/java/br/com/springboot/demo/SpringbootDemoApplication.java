d50fed1afbfa78eeab983352f01af40763ac4af6
nickkrasborges <nickkrasborges@gmail.com>
2019-11-10 18:43:18 -0300

    inclusão de cache.

--- src/main/java/br/com/springboot/demo/SpringbootDemoApplication.java
+++ src/main/java/br/com/springboot/demo/SpringbootDemoApplication.java
@@ -2,11 +2,13 @@ package br.com.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableCaching
 @EnableSwagger2
 public class SpringbootDemoApplication {
 
