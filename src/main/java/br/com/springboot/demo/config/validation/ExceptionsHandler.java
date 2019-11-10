09407646be147a7b19ae135147b030604845552e
nickkrasborges <nickkrasborges@gmail.com>
2019-11-10 16:12:21 -0300

    inclusão da validação de campos.

--- src/main/java/br/com/springboot/demo/config/validation/ExceptionsHandler.java
+++ src/main/java/br/com/springboot/demo/config/validation/ExceptionsHandler.java
@@ -4,20 +4,40 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
-public class ErrorHandler {
+public class ExceptionsHandler {
 
     @Autowired
-    private MessageSource source;
+    private MessageSource messageSource;
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity handle(NotFoundException exception){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handle(MethodArgumentNotValidException exception){
+        List<FieldException> fields = new ArrayList<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(f -> {
+            fields.add(
+                    FieldException.builder()
+                            .field(f.getField())
+                            .message(messageSource.getMessage(f, LocaleContextHolder.getLocale()))
+                            .build()
+            );
+        });
+
+        return ResponseEntity.badRequest().body(fields);
     }
 
     @ExceptionHandler(Exception.class)
