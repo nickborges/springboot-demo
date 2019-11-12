b01ec03a697dce70cd7266f756e6231fac4c7126
nickkrasborges <nickkrasborges@gmail.com>
2019-11-11 21:15:57 -0300

    alteração do cache

--- src/main/java/br/com/springboot/demo/SpringbootDemoApplication.java
+++ src/main/java/br/com/springboot/demo/SpringbootDemoApplication.java
@@ -8,7 +8,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
-@EnableCaching
 @EnableSwagger2
 public class SpringbootDemoApplication {
 
